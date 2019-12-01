3dd7b27b2888263e926c781371ab850825ecd10a
df5602 <dominik@fankhausers.ch>
2019-12-01 06:30:01 +0100

    Day 1, part 2.

--- 01-tyranny-of-the-rocket-equation/src/main.rs
+++ 01-tyranny-of-the-rocket-equation/src/main.rs
@@ -19,15 +19,34 @@ fn main() {
         }
     };
 
-    let fuel_requirements: u32 = input
+    let fuel_requirements: i32 = input
         .iter()
         .map(|&module_weight| calculate_fuel_weight(module_weight))
         .sum();
     println!("Total fuel requirement: {}", fuel_requirements);
+
+    let including_fuel: u32 = input
+        .iter()
+        .map(|&module_weight| calculate_fuel_weight_including_fuel(module_weight))
+        .sum();
+    println!("Fuel requirements (including fuel): {}", including_fuel);
 }
 
-fn calculate_fuel_weight(module_weight: u32) -> u32 {
-    module_weight / 3 - 2
+fn calculate_fuel_weight(module_weight: u32) -> i32 {
+    module_weight as i32 / 3 - 2
+}
+
+fn calculate_fuel_weight_including_fuel(module_weight: u32) -> u32 {
+    let mut total_fuel_weight = 0;
+    let mut fuel_weight = module_weight as i32;
+    loop {
+        fuel_weight = calculate_fuel_weight(fuel_weight as u32);
+        if fuel_weight <= 0 {
+            return total_fuel_weight;
+        } else {
+            total_fuel_weight += fuel_weight as u32;
+        }
+    }
 }
 
 #[cfg(test)]
@@ -40,5 +59,38 @@ mod tests {
         assert_eq!(2, calculate_fuel_weight(14));
         assert_eq!(654, calculate_fuel_weight(1969));
         assert_eq!(33583, calculate_fuel_weight(100756));
+    }
+
+    #[test]
+    fn fuel_weight_including_fuel() {
+        assert_eq!(2, calculate_fuel_weight_including_fuel(14));
+        assert_eq!(966, calculate_fuel_weight_including_fuel(1969));
+        assert_eq!(50346, calculate_fuel_weight_including_fuel(100756));
+    }
+
+    #[test]
+    fn part_1() {
+        let input: Vec<u32> = FileReader::new()
+            .split_lines()
+            .read_from_file("input.txt")
+            .unwrap();
+        let fuel_requirements: i32 = input
+            .iter()
+            .map(|&module_weight| calculate_fuel_weight(module_weight))
+            .sum();
+        assert_eq!(3399394, fuel_requirements);
+    }
+
+    #[test]
+    fn part_2() {
+        let input: Vec<u32> = FileReader::new()
+            .split_lines()
+            .read_from_file("input.txt")
+            .unwrap();
+        let including_fuel: u32 = input
+            .iter()
+            .map(|&module_weight| calculate_fuel_weight_including_fuel(module_weight))
+            .sum();
+        assert_eq!(5096223, including_fuel);
     }
 }
